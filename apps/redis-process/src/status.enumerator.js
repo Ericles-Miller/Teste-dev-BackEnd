e3ebb6783b6711e4340f2d9b61ba2a38a4d9b3a1
Éricles Miller <ericlesmiller15@gmail.com>
2024-11-26 14:14:19 -0300

    feat: att

---
+++ apps/redis-process/src/status.enumerator.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EStatus = void 0;
+var EStatus;
+(function (EStatus) {
+    EStatus["PROCESS"] = "Em andamento";
+    EStatus["COMPLETED"] = "Conclu\u00EDdo";
+    EStatus["ERROR"] = "Erro";
+})(EStatus || (exports.EStatus = EStatus = {}));
+//# sourceMappingURL=status.enumerator.js.map
