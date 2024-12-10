7cb976ffdc4e4d3b28e8f085b8eb5edc158501d2
Éricles Miller <ericlesmiller15@gmail.com>
2024-12-10 00:43:21 -0300

    feat: create realease

---
+++ apps/api/src/user/dto/create-user.dto.ts
@@ -0,0 +1,27 @@
+export class CreateUserDto {
+  id: number;
+
+  gender: string;
+
+  nameSet: string;
+
+  title: string;
+
+  givName: string;
+
+  surName: string;
+
+  streetAddress: string;
+
+  city: string;
+
+  emailAddress: string;
+
+  tropicalZodiac: string;
+
+  occupation: string;
+
+  vehicle: string;
+
+  countryFull: string;
+}
---
+++ apps/api/src/user/entities/user.entity.ts
@@ -0,0 +1,15 @@
+export class User {
+  id: number;
+  gender: string;
+  nameSet: string;
+  title: string;
+  givName: string;
+  surName: string;
+  streetAddress: string;
+  city: string;
+  emailAddress: string;
+  tropicalZodiac: string;
+  occupation: string;
+  vehicle: string;
+  countryFull: string;
+}
